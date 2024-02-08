d5bbd5fa867d311bcd92e6245837fcdf84cf511f
daikimorimoto <nanasinodonbei>
2024-02-08 22:10:09 +0900

    FizzBuzzプログラミング

---
+++ src/kadai/Review02.java
@@ -0,0 +1,28 @@
+package kadai;
+
+public class Review02 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        for (int i = 1; i <= 100; ++i) {
+
+            if (i % 15 == 0) {
+
+                System.out.println("FIZZBUZZ");
+
+            } else if (i % 3 == 0) {
+
+                System.out.println("FIZZ");
+
+            } else if (i % 5 == 0) {
+
+                System.out.println("BUZZ");
+
+            } else
+                System.out.println(i);
+
+        }
+    }
+
+}
